17dcbcb8e6a1c6909b9f9e50fa132b7452e78f33
Miguel Banuelos <mistergengar@gmail.com>
2014-10-08 09:22:14 -0700

    catching up

--- public_html/JS/snake.js
+++ public_html/JS/snake.js
@@ -7,7 +7,14 @@ var screenHeight;
 
 
 function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context =canvas.getContext("2d");
     
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
@@ -15,5 +22,5 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    
+    context.fillStyle = "rgb(37,184,83)";
 }
