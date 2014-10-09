0987fbf0eb3e7293f01f772df7ee3e2cc13f96f3
Miguel Banuelos <mistergengar@gmail.com>
2014-10-09 09:19:54 -0700

    I have done some work

--- public_html/JS/snake.js
+++ public_html/JS/snake.js
@@ -5,22 +5,26 @@ var context;
 var screenWidth;
 var screenHeight;
 
+gameInitialize();
+gameDraw();
+
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
-    context =canvas.getContext("2d");
-    
+    context = canvas.getContext("2d");
+
     screenWidth = window.innerWidth;
     screenHeight = window.innerHeight;
-    
+
     canvas.width = screenWidth;
     canvas.height = screenHeight;
 }
 
 function gameLoop() {
-    
+
 }
 
 function gameDraw() {
     context.fillStyle = "rgb(37,184,83)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
 }
