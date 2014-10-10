31bd20541d97b912054911d42ec864425675d128
Miguel Banuelos <mistergengar@gmail.com>
2014-10-10 09:20:03 -0700

    gh

--- public_html/JS/snake.js
+++ public_html/JS/snake.js
@@ -1,5 +1,6 @@
 var snake;
-
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
@@ -28,3 +29,30 @@ function gameDraw() {
     context.fillStyle = "rgb(37,184,83)";
     context.fillRect(0, 0, screenWidth, screenHeight);
 }
+
+function snakeInitialize(){
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    
+    for(var index = 0; index < snakeLength; index++){
+        snake.push({
+            x: index,
+            y:0
+        });
+    }
+}
+
+function snakeDraw(){
+    for(var index = 0; index < snake.length; index++) {
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+
+function snakeUpdate(){
+    
+}
+
+
+
